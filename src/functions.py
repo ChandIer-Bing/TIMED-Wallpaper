cdc08cde307913af75fb455a63844d53801f1939
ChandIer-Bing <53064450+ChandIer-Bing@users.noreply.github.com>
2020-07-16 22:40:22 -0400

    Update functions.py

---
+++ src/functions.py
@@ -0,0 +1,6 @@
+import datetime #this is used for the datetime.now() which returns date and time now
+import time     #this is needed for the scheduled module to work but currently no functions are explicitly used
+import schedule #this schedules the tasks so you can have recurring events at specific points in time
+import msvcrt   #this used to get constant imput from the keyboard without pressing enter
+import ctypes   #this i sused to change the wallpaper
+
