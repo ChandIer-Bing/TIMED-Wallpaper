4f6205bc06abb545709d5794070037160e2467ff
ChandIer-Bing <53064450+ChandIer-Bing@users.noreply.github.com>
2020-07-17 20:52:20 -0400

    added 3 function

--- src/functions.py
+++ src/functions.py
@@ -4,3 +4,25 @@ import schedule #this schedules the tasks so you can have recurring events at sp
 import msvcrt   #this used to get constant imput from the keyboard without pressing enter
 import ctypes   #this i sused to change the wallpaper
 
+def wallpaper_Changer(wallIndex,wallSource):
+    wallIndex=str(wallIndex)
+#    wallSource="D:\CODE\PYTHON\WALLPAPER_change\Big_Sur\macOS-Big-Sur-Daylight-Wallpaper-"+wallIndex+".jpg"
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, wallSource, 0)
+
+def check_Time(tA,tB):
+    daTime=str(datetime.datetime.now())
+    time=int(daTime[11:13])
+    if time>>tA and time<<tB:
+        return True
+    else:
+        return False
+
+def time_Dict(wallNum): #wallNum : number of wallpapers
+    global timeDict={}
+    for i in range(wallNum)
+        timeDict.setdefault("w"+str(wallNum),1)
+
+        
+
+
+
